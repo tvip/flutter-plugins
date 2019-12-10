c1e0a4b3822b9ff299d1fa5c44b4a85893c73011
Amir Hardon <amirh@users.noreply.github.com>
2019-12-09 16:48:07 -0800

    Update all pubspecs to work with the latest version of pub and Flutter (#2382)
    * Migrated all plugins to the new [plugin platforms manifest](https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms)
    * Removed `author:` and `authors:` fields from all pubspecs as the fields are deprecated, moved author names from various plugins to the repository's AUTHORS file.

--- packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player/ios/Classes/FLTVideoPlayerPlugin.m
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "VideoPlayerPlugin.h"
+#import "FLTVideoPlayerPlugin.h"
 #import <AVFoundation/AVFoundation.h>
 
 int64_t CMTimeToMillis(CMTime time) { return time.value * 1000 / time.timescale; }
