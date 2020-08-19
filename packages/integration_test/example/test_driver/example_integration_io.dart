d9d28c82e85b9a65445214fd389ec41bbc0e4ac8
Dan Field <dnfield@google.com>
2020-08-19 11:47:10 -0700

    Rename e2e -> integration_test (#2918)
    * e2e -> integration_test and associated version bumps

--- packages/integration_test/example/test_driver/example_integration_io.dart
+++ packages/integration_test/example/test_driver/example_integration_io.dart
@@ -5,15 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'dart:html' as html;
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:e2e/e2e.dart';
+import 'package:integration_test/integration_test.dart';
 
-import 'package:e2e_example/main.dart' as app;
+import 'package:integration_test_example/main.dart' as app;
 
 void main() {
-  E2EWidgetsFlutterBinding.ensureInitialized();
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('verify text', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     app.main();
@@ -21,13 +21,12 @@ void main() {
     // Trigger a frame.
     await tester.pumpAndSettle();
 
-    // Verify that platform is retrieved.
+    // Verify that platform version is retrieved.
     expect(
       find.byWidgetPredicate(
         (Widget widget) =>
             widget is Text &&
-            widget.data
-                .startsWith('Platform: ${html.window.navigator.platform}\n'),
+            widget.data.startsWith('Platform: ${Platform.operatingSystem}'),
       ),
       findsOneWidget,
     );
